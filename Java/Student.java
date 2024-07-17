01251f04533dacc555ce7a25c09c1bc32936c4be
Prashant <143874787+PRASHJANNUZ@users.noreply.github.com>
2024-07-17 16:51:17 +0530

    new files

---
+++ Java/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+
+
+    public static void msg()
+    {
+        System.out.println("Calling from msg method.... ");
+    } 
+    public static void main(String[] args) {
+        System.out.println("hiiii Yash");
+        
+
+    }
+
+    
+}
