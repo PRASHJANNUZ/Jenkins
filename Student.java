dcda8fcf39e74d78703aadc522d867ac22657e0e
Prashant <143874787+PRASHJANNUZ@users.noreply.github.com>
2024-07-17 16:42:09 +0530

    Created  Student class

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student 
+{
+    public static void msg()
+    {
+        System.out.println("Calling from msg method.... ");
+    } 
+    public static void main(String[] args) {
+        System.out.println("hiiii Yash");
+        
+
+    }
+}
