5dcc976adf6699c8d94d24bea3f2142070f2b891
Prashant <143874787+PRASHJANNUZ@users.noreply.github.com>
2024-07-17 16:57:20 +0530

    Now working properly

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ public class Main
         System.out.println("hiiii prashant");
         System.out.println("hiiii Tanmay");
         Student.msg();
+        System.out.println("Yeah now its working properly....");
+        Student.msg();
         
     }
 }
